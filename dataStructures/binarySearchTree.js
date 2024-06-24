b2e7d94bb01dc69a47aa24eea30c35048962790b
GustavoDoosAnjos <anjos.gu@gmail.com>
2024-06-24 08:30:55 -0300

    add tree traversal methods

--- dataStructures/binarySearchTree.js
+++ dataStructures/binarySearchTree.js
@@ -58,14 +58,64 @@ class BinarySearchTree {
     }
     return false;
   }
+
+  breadthFirstSearch() {
+    let queue = [];
+    let visited = [];
+    let current;
+
+    queue.push(this.root);
+    while (queue.length > 0) {
+      current = queue.shift();
+      visited.push(current.value);
+      if (current.left !== null) {
+        queue.push(current.left);
+      }
+      if (current.right !== null) {
+        queue.push(current.right);
+      }
+    }
+    return visited;
+  }
+
+  depthFirstSearchPreOrder() {
+    let visited = [];
+
+    function traverse(node) {
+      visited.push(node.value);
+
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return visited;
+  }
+
+  depthFirstSearchPostOrder() {
+    let visited = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+
+      visited.push(node.value);
+    }
+
+    traverse(this.root);
+    return visited;
+  }
+
+  depthFirstSearchInOrder() {
+    let visited = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      visited.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return visited;
+  }
 }
-
-const bst = new BinarySearchTree();
-
-bst.insert(10);
-bst.insert(12);
-bst.insert(25);
-bst.insert(250);
-bst.insert(3);
-
-bst.find(250);
